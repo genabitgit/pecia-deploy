9e67608e19ef2528d8998572f88b11fd4931c73e
gena <gennadygpost@gmail.com>
2018-03-26 09:31:28 +0300

    change_file

--- startsync.bash
+++ startsync.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 set CYGWIN=nontsec
-rsync -avz --chmod=ugo=rwX --omit-dir-times --delete -e "ssh -i ../../../id_rsa" "./publish/" teamcity@teamcitypecia.ddns.net:/var/www/
+rsync -av -og --no-group --omit-dir-times --delete -e "ssh -i ../../../id_rsa" "./publish/" teamcity@teamcitypecia.ddns.net:/var/www/
